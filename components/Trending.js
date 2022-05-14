1959f3bca9c6e4d8aea89b2e0ec03df7a4706fab
miarta <miarta.igede@gmail.com>
2022-05-15 00:18:53 +0800

    fixing data

--- components/Trending.js
+++ components/Trending.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import fire from '../assets/fire.png'
 import btc from '../assets/btc.png'
 import usdt from '../assets/usdt.png'
@@ -9,12 +8,45 @@ import recent from '../assets/recent.png'
 import ReactSwitch from 'react-switch'
 import Rate from "./cmc-table/Rate";
 import TrendingCard from './TrendingCard'
+import Shiba from '../assets/shiba.png'
+import Tera from '../assets/tera.png'
+import xrp from '../assets/xrp.png' 
+import Binanace from '../assets/bnb.png'
+import Cardanao from '../assets/cardano.png'
+import Solana from '../assets/solana.png'
+ 
 
 const styles = {
     trendingWrapper: `mx-auto max-w-screen-xl px-4`,
     h1: `text-3xl text-white`,
     flexCenter: `flex items-center`,
 }
+const RecentlyAded = [
+    {
+        number:1,
+        symbol:'BNB',
+        name:'Binance Coin',
+        icon:Binanace,
+        isIncrement:true,
+        rate:'4.53%',
+
+    },
+    {
+        number:2,
+        symbol:'SOL',
+        name:'Solana',
+        icon:Solana,
+        isIncrement:false,
+        rate:'11.53%',
+    },{
+        number:3,
+        symbol:'ADA',
+        name:'Cardano',
+        icon:Cardanao,
+        isIncrement:false,
+        rate:'6.88%',
+    }
+];
 
 const trendingData = [
     {
@@ -40,6 +72,33 @@ const trendingData = [
         icon:eth,
         isIncrement:true,
         rate:'2.53%',
+    }
+];
+
+const bigestGain = [
+    {
+        number:1,
+        symbol:'XRP',
+        name:'Ripple',
+        icon:xrp,
+        isIncrement:true,
+        rate:'6.3%',
+
+    },
+    {
+        number:2,
+        symbol:'TERA',
+        name:'Tera',
+        icon:Tera,
+        isIncrement:false,
+        rate:'9.53%',
+    },{
+        number:3,
+        symbol:'SHIB',
+        name:'Shiba Inu',
+        icon:Shiba,
+        isIncrement:false,
+        rate:'8.53%',
     }
 ];
 
@@ -69,8 +128,8 @@ function Trending() {
           <br/>
           <div className={styles.flexCenter}>
               <TrendingCard title='Trending' icon={fire} trendingData={trendingData} />
-              <TrendingCard title='Bigest Gainers' icon={gainers} trendingData={trendingData} />
-              <TrendingCard title='Recently Added' icon={recent} trendingData={trendingData} />
+              <TrendingCard title='Bigest Gainers' icon={gainers} trendingData={bigestGain} />
+              <TrendingCard title='Recently Added' icon={recent} trendingData={RecentlyAded} />
           </div>
       </div>
     </div>
